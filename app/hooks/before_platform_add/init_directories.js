235000d121d9447dcea3170fe43ad3c225e0e4af
Crowley <castiel.zyf@gmail.com>
2015-04-09 21:22:32 +0800

    recreate the app
    using yeoman to recreate the app

--- app/hooks/before_platform_add/init_directories.js
+++ app/hooks/before_platform_add/init_directories.js
@@ -4,20 +4,16 @@
  * On a fresh clone, the local platforms/ and plugins/ directories will be
  * missing, so ensure they get created before the first platform is added.
  */
-var fs = require('fs');
+var mkdirp = require('mkdirp');
 var path = require('path');
 
 var platformsDir = path.resolve(__dirname, '../../platforms');
 var pluginsDir = path.resolve(__dirname, '../../plugins');
 
-try {
-  fs.mkdirSync(platformsDir, function (err) {
-    if (err) { console.error(err); }
-  });
-} catch(ex) {}
+mkdirp(platformsDir, function (err) {
+  if (err) { console.error(err); }
+});
 
-try {
-  fs.mkdirSync(pluginsDir, function (err) {
-    if (err) { console.error(err); }
-  });
-} catch(ex) {}
+mkdirp(pluginsDir, function (err) {
+  if (err) { console.error(err); }
+});
