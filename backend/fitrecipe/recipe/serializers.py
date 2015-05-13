cd3ba0ec11c69127e45e1b05f6d1e4c2848db162
Haotian Chai <haotianchai@wandoujia.com>
2015-05-13 21:41:53 +0800

    recipe的searilizer现在可以直接访问到lables啦！

--- backend/fitrecipe/recipe/serializers.py
+++ backend/fitrecipe/recipe/serializers.py
@@ -3,15 +3,10 @@
 # @Author: chaihaotian
 # @Date:   2015-04-26 15:52:14
 # @Last Modified by:   chaihaotian
-# @Last Modified time: 2015-05-13 20:32:12
+# @Last Modified time: 2015-05-13 21:40:38
 from rest_framework import serializers
 
 from .models import Recipe, Component, Procedure, Label, Ingredient, Nutrition
-
-
-class RecipeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Recipe
 
 
 class ComponentSerializer(serializers.ModelSerializer):
@@ -27,6 +22,7 @@ class ProcedureSerializer(serializers.ModelSerializer):
 class LabelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Label
+        fields = ('id', 'name', 'type')
 
 
 class IngredientSerializer(serializers.ModelSerializer):
@@ -37,3 +33,14 @@ class IngredientSerializer(serializers.ModelSerializer):
 class NutritionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Nutrition
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    meat_labels = LabelSerializer(many=True)  # 我不要看到那些created_time, updated_time
+    time_labels = LabelSerializer(many=True)
+    effect_labels = LabelSerializer(many=True)
+    other_labels = LabelSerializer(many=True)
+
+    class Meta:
+        model = Recipe
+        depth = 1
