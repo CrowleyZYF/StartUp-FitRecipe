084f918432d014b5d74ba366728e9eab3af74983
Haotian Chai <haotianchai@wandoujia.com>
2015-05-15 20:29:11 +0800

    删掉不用的代码

--- backend/fitrecipe/recipe/views.py
+++ backend/fitrecipe/recipe/views.py
@@ -3,7 +3,7 @@
 # @Author: chaihaotian
 # @Date:   2015-04-26 15:44:45
 # @Last Modified by:   chaihaotian
-# @Last Modified time: 2015-05-15 19:10:45
+# @Last Modified time: 2015-05-15 20:28:27
 
 from base.views import BaseView
 from .models import Recipe, Component, Ingredient, Label, Nutrition, Procedure
@@ -28,106 +28,6 @@ class RecipeDetail(BaseView):
         '''
         recipe = self.get_object(Recipe, pk)
         serializer = RecipeSerializer(recipe)
-        return self.success_response(serializer.data)
-
-
-class ComponentList(BaseView):
-    def get(self, request, format=None):
-        '''
-        List all Components
-        '''
-        components = Component.objects.all()
-        serializer = ComponentSerializer(components, many=True)
-        return self.success_response(serializer.data)
-
-
-class ComponentDetail(BaseView):
-    def get(self, request, pk, format=None):
-        '''
-        return a specific component.
-        '''
-        component = self.get_object(Component, pk)
-        serializer = ComponentSerializer(component)
-        return self.success_response(serializer.data)
-
-
-class IngredientList(BaseView):
-    def get(self, request, format=None):
-        '''
-        List all Ingredients
-        '''
-        ingredients = Ingredient.objects.all()
-        serializer = IngredientSerializer(ingredients, many=True)
-        return self.success_response(serializer.data)
-
-
-class IngredientDetail(BaseView):
-    def get(self, request, pk, format=None):
-        '''
-        return a specific Ingredient.
-        '''
-        ingredient = self.get_object(Ingredient, pk)
-        serializer = IngredientSerializer(ingredient)
-        return self.success_response(serializer.data)
-
-
-class LabelList(BaseView):
-    def get(self, request, format=None):
-        '''
-        List all Labels
-        '''
-        labels = Label.objects.all()
-        serializer = LabelSerializer(labels, many=True)
-        return self.success_response(serializer.data)
-
-
-class LabelDetail(BaseView):
-    def get(self, request, pk, format=None):
-        '''
-        return a specific Label.
-        '''
-        label = self.get_object(Label, pk)
-        serializer = LabelSerializer(label)
-        return self.success_response(serializer.data)
-
-
-class NutritionList(BaseView):
-    def get(self, request, format=None):
-        '''
-        List all Nutritions
-        '''
-        nutritions = Nutrition.objects.all()
-        serializer = NutritionSerializer(nutritions, many=True)
-        return self.success_response(serializer.data)
-
-
-class NutritionDetail(BaseView):
-    def get(self, request, pk, format=None):
-        '''
-        return a specific Nutrition.
-        '''
-        nutrition = self.get_object(Nutrition, pk)
-        serializer = NutritionSerializer(nutrition)
-        return self.success_response(serializer.data)
-
-
-class ProcedureList(BaseView):
-    def get(self, request, format=None):
-        '''
-        List all Procedures
-        '''
-        procedures = Procedure.objects.all()
-        serializer = ProcedureSerializer(procedures, many=True)
-        return self.success_response(serializer.data)
-
-
-class ProcedureDetail(BaseView):
-    def get(self, request, pk, format=None):
-        '''
-        return a specific Procedure.
-        '''
-        procedure = self.get_object(Procedure, pk)
-        serializer = ProcedureSerializer(procedure)
         return self.success_response(serializer.data)
 
 
