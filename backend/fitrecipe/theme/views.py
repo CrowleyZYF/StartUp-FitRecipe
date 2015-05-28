56042707c7a45564a969aec6692159dac2aa3d50
Haotian Chai <haotianchai@wandoujia.com>
2015-05-28 17:56:19 +0800

    Theme Detail 接口不返回菜谱列表，只包含数量。新增一个接口用于获取主题下的菜谱，带分页。文档已更新

--- backend/fitrecipe/theme/views.py
+++ backend/fitrecipe/theme/views.py
@@ -3,10 +3,11 @@
 # @Author: chaihaotian
 # @Date:   2015-05-28 13:16:10
 # @Last Modified by:   chaihaotian
-# @Last Modified time: 2015-05-28 15:57:45
+# @Last Modified time: 2015-05-28 17:44:24
 from base.views import BaseView
 from .serializers import ThemeSerializer
 from .models import Theme
+from recipe.serializers import RecipeSerializer
 
 
 # Create your views here.
@@ -16,7 +17,7 @@ class ThemeDetail(BaseView):
         reutrn a specific Theme
         '''
         theme = self.get_object(Theme, pk)
-        serializer = ThemeSerializer(theme, context={'simple': False})
+        serializer = ThemeSerializer(theme)
         return self.success_response(serializer.data)
 
 
@@ -26,5 +27,18 @@ class ThemeList(BaseView):
         Theme List
         '''
         themes = Theme.objects.all()
-        serializer = ThemeSerializer(themes, context={'simple': True}, many=True)
+        serializer = ThemeSerializer(themes, many=True)
+        return self.success_response(serializer.data)
+
+
+class ThemeRecipeList(BaseView):
+    def get(self, request, theme_pk, format=None):
+        '''
+        return recipes of theme whose id is theme_pk
+        '''
+        num = self.request.GET.get('num', 5)
+        start = self.request.GET.get('start', 0)
+        t = Theme.objects.get(pk=theme_pk)
+        recipes = t.get_recipes(start, num)
+        serializer = RecipeSerializer(recipes, context={'simple': True}, many=True)
         return self.success_response(serializer.data)
