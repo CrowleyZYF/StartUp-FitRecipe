f9089cb4b6470830ccee8772fd8fec026562700c
Crowley <castiel.zyf@gmail.com>
2015-08-11 19:50:46 +0800

    finish the interface of the plan_test

---
+++ app/FitRecipe/app/src/main/java/cn/fitrecipe/android/PlanTest/Q11Fragment.java
@@ -0,0 +1,22 @@
+package cn.fitrecipe.android.PlanTest;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cn.fitrecipe.android.R;
+
+/**
+ * Created by 奕峰 on 2015/4/11.
+ */
+public class Q11Fragment extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_plan_test_11, container, false);
+    }
+}
